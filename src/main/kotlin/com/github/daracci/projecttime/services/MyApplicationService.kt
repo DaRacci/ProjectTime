3179940cd792b85130469f9acd464b42fd54926e
GitHub Action <action@github.com>
2021-12-28 09:45:22 +0000

    Template cleanup

--- src/main/kotlin/com/github/daracci/projecttime/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/daracci/projecttime/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.daracci.projecttime.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.daracci.projecttime.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/daracci/projecttime/services/MyProjectService.kt
+++ src/main/kotlin/com/github/daracci/projecttime/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.daracci.projecttime.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.daracci.projecttime.MyBundle
 
 class MyProjectService(project: Project) {
 
